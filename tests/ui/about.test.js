8e4ec219e55e4916c2324b09d25453667782463b
Georgi Kolev <g.kolev15@gmail.com>
2023-12-16 12:51:39 +0200

    fixed urls

--- tests/ui/about.test.js
+++ tests/ui/about.test.js
@@ -1,7 +1,7 @@
 const { test, expect } = require('@playwright/test');
 
 test('Check about page', async ({ page }) => {
-  await page.goto('https://gkolev15-exam-prep.onrender.com//about');
+  await page.goto('https://gkolev15-exam-prep.onrender.com/about');
   const heading = await page.$('h1');
   const text = await heading.textContent();
   expect(text).toBe('About');
--- tests/ui/home.test.js
+++ tests/ui/home.test.js
@@ -1,7 +1,7 @@
 const { test, expect } = require('@playwright/test');
 
 test('Check home page', async ({ page }) => {
-    await page.goto('https://gkolev15-exam-prep.onrender.com//');
+    await page.goto('https://gkolev15-exam-prep.onrender.com');
     const heading = await page.$('h1');
     const text = await heading.textContent();
     expect(text).toContain('Books Collection');  
